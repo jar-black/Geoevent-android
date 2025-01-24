7bbd95c9df4c2618e5f89bad8fdb5cee13a8abe1
Johan Allander <johan.allander@pagero.com>
2025-01-24 15:34:02 +0100

    Add basic frame for android App

---
+++ app/src/main/java/com/geoevent/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.geoevent.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "[DASHBOARD]"
+    }
+    val text: LiveData<String> = _text
+}
