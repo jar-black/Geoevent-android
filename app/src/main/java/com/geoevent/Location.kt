ae4adb33ad7f0d1fcd8dff2856f802d116f4b535
jar-black <vaken@protonmail.com>
2025-02-16 15:42:49 +0100

    Add location lookup to android app (#2)
    Co-authored-by: Johan Allander <johan.allander@pagero.com>

---
+++ app/src/main/java/com/geoevent/Location.kt
@@ -0,0 +1,52 @@
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.os.Looper
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+
+class LocationHelper(context: Context) {
+    private val fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
+
+    private var locationCallback: LocationCallback? = null
+
+    @SuppressLint("MissingPermission")
+    fun getLatestLocation(): Location {
+        return fusedLocationClient.lastLocation.result
+    }
+
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates(onLocationReceived: (Location) -> Unit) {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000)
+            .setWaitForAccurateLocation(false)
+            .setMinUpdateIntervalMillis(5000)
+            .setMaxUpdateDelayMillis(10000)
+            .build()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let { location ->
+                    onLocationReceived(location)
+                }
+            }
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback!!,
+            Looper.getMainLooper()
+        )
+    }
+
+    fun stopLocationUpdates() {
+        locationCallback?.let {
+            fusedLocationClient.removeLocationUpdates(it)
+            locationCallback = null
+        }
+    }
+}
